8c380075358bb45b48e7ca9a7890a874ffdd3593
abidknashtech <127501072+abidknashtech@users.noreply.github.com>
2024-01-24 19:05:50 +0530

    Update apps-deployment-script.sh

--- apps-deployment-script.sh
+++ apps-deployment-script.sh
@@ -16,10 +16,10 @@ build_and_deploy_service(){
    if [  $SERVICE_NAME != "car-ui" ]; then
        #mvn verify sonar:sonar
        #mvn verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=nashtech
-       #mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
+       mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
        echo "----------testing1------------------------"
-       cat $HOME/.m2/settings.xml | base64
-       mvn clean install -s $HOME/.m2/settings.xml
+       #cat $HOME/.m2/settings.xml | base64
+       #mvn clean install -s $HOME/.m2/settings.xml
        echo "-------------$SERVICE_NAME deployed ----------"
    fi
 }
@@ -34,8 +34,8 @@ build_and_deploy_service1(){
    if [  $SERVICE_NAME != "car-ui" ]; then
       # mvn verify sonar:sonar
        # mvn verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=nashtech
-       #mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
-       mvn clean install -X
+       mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
+       #mvn clean install -X
        #mvn clean install  -s $HOME/.m2/settings.xml
    fi
    echo "---------packaging done, start docker build-----------"
