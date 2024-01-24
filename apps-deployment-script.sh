0f57150c4c7af6defce85180dad6148ce8fa0ca4
abidknashtech <127501072+abidknashtech@users.noreply.github.com>
2024-01-24 14:40:05 +0530

    Update apps-deployment-script.sh

--- apps-deployment-script.sh
+++ apps-deployment-script.sh
@@ -17,6 +17,20 @@ build_and_deploy_service(){
       # mvn verify sonar:sonar
        # mvn verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=nashtech
        mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
+       echo "-------------$SERVICE_NAME deployed ----------"
+}
+
+build_and_deploy_service1(){
+
+   SERVICE_NAME=$1
+   CLUSTER_NAME=$2
+   DEPLOYMENT_NAME=$3
+   echo "---------build and deploy $SERVICE_NAME-----------"
+   cd "$SERVICE_NAME" || exit
+   if [  $SERVICE_NAME != "car-ui" ]; then
+      # mvn verify sonar:sonar
+       # mvn verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=nashtech
+       mvn clean install -s $GITHUB_WORKSPACE/settings.xml -X
        #mvn clean install -X
        #mvn clean install  -s $HOME/.m2/settings.xml
    fi
