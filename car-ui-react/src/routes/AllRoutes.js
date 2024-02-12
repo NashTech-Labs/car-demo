0c10c58cc0aa38782df9a96d3254de829f8dc2bf
DeepaMittal <deepamittalmca@gmail.com>
2024-02-12 20:15:39 +0530

    Added cart feature

--- car-ui-react/src/routes/AllRoutes.js
+++ car-ui-react/src/routes/AllRoutes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { CartPage } from "../pages";
+import {ProductsList} from "../pages/Products/ProductsList";
 
 export const AllRoutes = () => {
   return (
     <>
     <Routes>
-        <Route path="cart" element={<CartPage />} />
+        <Route path="/products" element={<ProductsList />} />
+        <Route path="/cart" element={<CartPage />} />
     </Routes>
     </>
   )
