4d416037dd535305523cbc13635cbfc40fcc81de
deepak1067 <deepak@knoldus.com>
2024-02-14 22:44:41 +0530

    added PageNotFound page

--- car-ui-react/src/pages/index.js
+++ car-ui-react/src/pages/index.js
@@ -1,3 +1,4 @@
 export { DashboardPage } from "./Dashboard/DashboardPage";
 export { CartPage } from "./Cart/CartPage";
 export { HomePage } from "./Home/HomePage";
+export { PageNotFound } from "./PageNotFound";
--- car-ui-react/src/routes/AllRoutes.js
+++ car-ui-react/src/routes/AllRoutes.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { CartPage } from "../pages";
 import { ProductsList}  from "../pages/Products/ProductsList";
 import { HomePage } from "../pages/Home/HomePage";
+import { PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
   return (
@@ -10,6 +11,7 @@ export const AllRoutes = () => {
         <Route path="/" element={<HomePage/>} />
         <Route path="/products" element={<ProductsList />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="*" element={<PageNotFound />} />
     </Routes>
     </>
   )
